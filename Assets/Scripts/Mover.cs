a440548906dea6c73b0dfe52618227c5a440253b
kovalen1618 <kovalen1.618@gmail.com>
2023-01-31 15:18:44 -0600

    Implemented click-to-move feature

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -6,21 +6,31 @@ using UnityEngine.AI;
 public class Mover : MonoBehaviour
 {
     // 1. Serialize a Transform target variable
-
     [SerializeField] Transform target;
-
-    // 3. Variable of type Ray that holds the last ray shot
-    Ray lastRay;
 
     void Update()
     {
-        // 4. Check for mouse input to give lastRay value
+        // 4. Check for mouse input to move player to cursor
         if (Input.GetMouseButtonDown(0))
         {
-            lastRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            MoveToCursor();
         }
+    }
 
-        // 2. Within Update method, get NavMeshAgent component and set its destination value as the target's current position
-        GetComponent<NavMeshAgent>().destination = target.position;
+    // 5.
+    private void MoveToCursor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        // 6. Passing in ray and at the same time returning hit
+        bool hasHit = Physics.Raycast(ray, out hit);
+
+        // 7. If we hit something, move to where the ray hit
+        if (hasHit)
+        {
+            // 2. Within Update method, get NavMeshAgent component and set its destination value as the target's current position
+            GetComponent<NavMeshAgent>().destination = hit.point;
+        }
     }
 }
